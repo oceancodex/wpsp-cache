8c9390d30332ca03699503b61b1b89ed78f6e558
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-11 13:39:11 +0700

    KDN-PC-01: Update

--- src/Adapter.php
+++ src/Adapter.php
@@ -14,7 +14,7 @@ class Adapter extends BaseInstances {
 
 	private string $store;
 
-	public function afterConstruct(): void {
+	public function afterConstruct() {
 		$this->store = $this->funcs->_env('CACHE_STORE', true);
 	}
 
